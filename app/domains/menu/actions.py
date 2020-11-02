1c5c05081473fc471651b15579459b757ce5fdca
tiago.souza <tiago.souza@hbsis.com.br>
2020-11-02 17:15:14 -0300

    Fixed the merge

--- app/domains/menu/actions.py
+++ app/domains/menu/actions.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from starlette.exceptions import HTTPException
 from starlette.status import HTTP_404_NOT_FOUND
 
@@ -6,37 +5,16 @@ from app.database.repository import Repository, get_repository
 from app.domains.menu.model import Menu
 from app.routes.menu.menu_request import CreateMenuRequest
 from app.routes.menu.menu_response import MenuResponse
-=======
-from fastapi import Depends
-from starlette.exceptions import HTTPException
-from starlette.status import HTTP_404_NOT_FOUND
-
-from app import CreateMenuRequest, Repository, get_repository
-from app.domains.menu.model import Menu
->>>>>>> feature/create_update_menu
-
 
 def update_menu(
         id: int,
         create_menu_request: CreateMenuRequest,
-<<<<<<< HEAD
         repository: Repository = next(get_repository())
 ):
     menu = repository.get_by_id(Menu, id)
-    print(menu)
     if not menu:
         raise HTTPException(status_code=HTTP_404_NOT_FOUND)
     menu.update(create_menu_request)
     repository.save(menu)
     response = MenuResponse.from_domain(menu)
     return response
-=======
-        repository: Repository = Depends(get_repository)
-):
-    menu = repository.get_by_id(Menu, id)
-    if not menu:
-        raise HTTPException(status_code=HTTP_404_NOT_FOUND)
-    menu.update(create_menu_request)
-    repository.insert(menu)
-    return menu
->>>>>>> feature/create_update_menu
--- app/routes/menu/menu_routes.py
+++ app/routes/menu/menu_routes.py
@@ -2,14 +2,10 @@ from fastapi import APIRouter, Depends
 from starlette.exceptions import HTTPException
 from starlette.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_404_NOT_FOUND
 
-<<<<<<< HEAD
-from app.database.repository import  get_repository, Repository
-from app.routes.menu.menu_request import CreateMenuRequest
-=======
-from app import CreateMenuRequest, get_repository, Repository
->>>>>>> feature/create_update_menu
+from app.database.repository import Repository, get_repository
 from app.domains.menu.actions import update_menu
 from app.domains.menu.model import Menu
+from app.routes.menu.menu_request import CreateMenuRequest
 
 router = APIRouter()
 
@@ -31,7 +27,6 @@ def get_by_id(
         raise HTTPException(status_code=HTTP_404_NOT_FOUND)
     return response
 
-<<<<<<< HEAD
 @router.post('/', status_code=HTTP_201_CREATED)
 def create_menu(
         create_menu_request: CreateMenuRequest,
@@ -43,20 +38,7 @@ def create_menu(
 @router.put('/{id}')
 def update(
         id: int,
-        request: CreateMenuRequest,
-):
-    response = update_menu(id, request)
-    return response
-=======
-@router.put('/{id}')
-def update(
-        id: int,
         create_menu_request: CreateMenuRequest,
 ):
     response = update_menu(id, create_menu_request)
     return response
-
-
-
-
->>>>>>> feature/create_update_menu
