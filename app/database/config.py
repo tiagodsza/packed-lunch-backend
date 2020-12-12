eec62721bf6dbcef879aade88cd001d00973fd8a
Tiago de Souza <tiagodsza@gmail.com>
2020-12-12 15:50:50 -0300

    Changed the database to Postgres

--- app/database/config.py
+++ app/database/config.py
@@ -7,14 +7,7 @@ from sqlalchemy.orm import sessionmaker
 import config
 
 Base = declarative_base()
-parameters = (
-    f'DRIVER={config.DRIVER};'
-    f'SERVER={config.SERVER};'
-    f'DATABASE={config.DATABASE};'
-    f'UID={config.DB_USER};'
-    f'PWD={config.DB_PASSWORD};'
-)
-url_parameters_db = quote_plus(parameters)
-URL_DB = f'mssql+pyodbc:///?odbc_connect=%s' % url_parameters_db
-engine = create_engine(URL_DB)
+
+print(config.URL_DB)
+engine = create_engine(config.URL_DB)
 Session = sessionmaker(bind=engine)
